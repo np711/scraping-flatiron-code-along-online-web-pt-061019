8681fbda89af5e3814d96fca39278dc5901886f7
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-08-16 15:20:23 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,15 +4,6 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
-  def get_page 
-    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-    binding.pry 
-    
-  end 
-  
-end 
-
-Scraper.new.get_page 
   
     def print_courses
     self.make_courses
@@ -26,6 +17,3 @@ Scraper.new.get_page
   end
   
 end
-
-
-
